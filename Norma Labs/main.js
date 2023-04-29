3e20d11b2f7ed8e6eb05c747e3d99bb0d67bdb16
DeividMesquita <115595872+DeividMesquita@users.noreply.github.com>
2023-04-29 16:46:53 -0300

    Add files via upload

---
+++ Norma Labs/main.js
@@ -0,0 +1,14 @@
+function redirecionar(){
+    window.open("https://www.linkedin.com/company/normalabs-hub/?originalSubdomain=br");
+    
+}
+function redirecionarInsta(){
+    window.open("https://instagram.com/normalabs.hub?igshid=YmMyMTA2M2Y=")
+}
+function redirecionarGit(){
+    window.open("https://github.com/DeividMesquita")
+}
+function redirecionarYoutube(){
+    window.open("https://youtu.be/gA6xGM9wpHo");
+    
+}
